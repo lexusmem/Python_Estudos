9c81aa9416a5f3e9c54b74f326b36589581b9d66
Alex Sousa <lexusmem@gmail.com>
2023-12-09 11:05:09 -0300

    Nova aulas do curso alura

--- Python_Aluara/alura_avaliador_test.py
+++ Python_Aluara/alura_avaliador_test.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest import TestCase
-from alura_testes import Avaliador, Lance, Leilao, Usuarios
+from alura_testes import Lance, Leilao, Usuarios
 
 
 class TestAvaliador(TestCase):
@@ -17,59 +17,47 @@ class TestAvaliador(TestCase):
 
     def test_maior_e_menor_valor_de_lance_quando_adicionados_em_ordem_crescente(self):
 
-        self.leilao.lances.append(self.lance_laura)
-        self.leilao.lances.append(self.lance_alex)
-
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
+        self.leilao.propoe(self.lance_laura)
+        self.leilao.propoe(self.lance_alex)
 
         menor_valor_esperado = 100.00
         maior_valor_esperado = 150.00
 
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
     def test_maior_e_menor_valor_de_lance_quando_adicionados_em_ordem_decrescente(self):
 
-        self.leilao.lances.append(self.lance_alex)
-        self.leilao.lances.append(self.lance_laura)
-
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
+        self.leilao.propoe(self.lance_alex)
+        self.leilao.propoe(self.lance_laura)
 
         maior_valor_esperado = 150.00
         menor_valor_esperado = 100.00
 
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
 
     def test_deve_retornar_o_mesmo_valor_para_menor_e_maior_lance_quando_existir_um_unico_lance(self):
 
-        self.leilao.lances.append(self.lance_laura)
+        self.leilao.propoe(self.lance_laura)
 
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-
-        self.assertEqual(150, avaliador.menor_lance)
-        self.assertEqual(150, avaliador.maior_lance)
+        self.assertEqual(150, self.leilao.menor_lance)
+        self.assertEqual(150, self.leilao.maior_lance)
 
     def test_maior_e_menor_valor_de_mais_dois_lance(self):
         pamela = Usuarios('Pamela')
 
         lance_pamela = Lance(pamela, 250.00)
 
-        self.leilao.lances.append(self.lance_alex)
-        self.leilao.lances.append(self.lance_laura)
-        self.leilao.lances.append(lance_pamela)
-
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
+        self.leilao.propoe(self.lance_alex)
+        self.leilao.propoe(self.lance_laura)
+        self.leilao.propoe(lance_pamela)
 
         maior_valor_esperado = 250.00
         menor_valor_esperado = 100.00
 
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
 
 
 if __name__ == '__main__':
