f80e3af7efd51cd6768ad01e14d61e1790280f08
Alex Sousa <lexusmem@gmail.com>
2024-02-23 22:01:54 -0300

    novos estudos do livro

--- Livro_Pense em Python/Capitulo 4 - Estudo de Caso - Projeto de Interface.py
+++ Livro_Pense em Python/Capitulo 4 - Estudo de Caso - Projeto de Interface.py
@@ -1,24 +1,44 @@
 import turtle
+
+# Este estudo de caso demonstra o processo de criação de funções que operam
+# simultaneamente.
+
+# Apresenta o módulo turtle que permite criar imagens usando [turtle graphics]
+
+# Módulo turtle
+# import turtle
+
+
+# módulo turtle (t minusculo) função Turtle(T maiúsculo).
+# Que cria um objeto Turtle, que está atribuído a variável Bob
 bob = turtle.Turtle()
-bob.lt(90)
-bob.fd(50)
-bob.rt(90)
-bob.fd(10)
-bob.rt(90)
-bob.fd(50)
-bob.lt(90)
-bob.fd(10)
-bob.lt(90)
-bob.fd(50)
-
-bob.rt(90)
-bob.fd(10)
-bob.rt(90)
-bob.fd(50)
-bob.lt(90)
-bob.fd(10)
-bob.lt(90)
-bob.fd(50)
-
+bob2 = turtle.Turtle()
 print(bob)
+
+# uma vez criado o Turtle pode chamar um método para move-lo para frente.
+# .fd(valor) - mover para frente
+# .bk(valor) - mover para para trás
+# O método fd e bk é uma distância em pixeis, então depende do tamanho
+# real da tela.
+bob.fd(100)
+
+# .lt(valor) - vira a esquerda
+# .rt(valor) - vira a direita
+# O método lt e rt o argumento é um angulo em graus.
+bob.lt(90)
+bob.fd(100)
+
+# Cada turtle segura uma caneta podendo a caneta esta abaixada (deixa rastro) .pd()
+# E para cima (não deixa rastro) .pu()
+
+bob2.pu()
+bob2.bk(100)
+bob2.lt(90)
+bob2.pd()
+bob2.fd(100)
+
+# mainloop diz que a janela deve esperar o usuário fechar a janela.
+# Deve fica no fim do código.
 turtle.mainloop()
+
+# Parei - 4.2 Repetição Simples
